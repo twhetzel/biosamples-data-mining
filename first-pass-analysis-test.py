efbc71ec572c49bfa3d0ba4bc04a776aaa9dafb6
Trish Whetzel <Trish>
2017-06-20 12:36:04 +0100

    refactor

--- first-pass-analysis-test.py
+++ first-pass-analysis-test.py
@@ -1,14 +1,12 @@
 #pip install neo4j-driver
-
 from neo4j.v1 import GraphDatabase, basic_auth
 
-import csv
-
 import datetime
-
 from functools import wraps
 from time import time
 
+import csv
+import os.path
 
 
 def timing(f):
@@ -20,8 +18,8 @@ def timing(f):
         ts = time()
         result = f(*args, **kw)
         te = time()
-        print 'Function: %r args:[%r, %r] took: %2.4f sec' % \
-          (f.__name__, args, kw, te-ts)
+        print 'Function: %r args:[%r, %r] took: %2.2f sec, %2.2f min' % \
+          (f.__name__, args, kw, te-ts, (te-ts)/60)
         return result
     return wrap
 
@@ -35,29 +33,71 @@ def get_timestamp():
 
 
 @timing
-def get_all_attribute_types(driver):
+def get_all_attribute_types_and_values(driver):
     """ 
-    Get all unique attribute types. 
+    Get all unique attribute types and values for each type. 
     """
-    print "Generating spreadsheet of unique attribute types..."
+    print "Generating file of unique attribute types..."
     
+    results = []
+
     TIMESTAMP = get_timestamp()
-    with open("TEST-attr_common_"+TIMESTAMP+".csv", "w") as outfile:
+    with open("unique_attr_types_"+TIMESTAMP+".csv", "w") as outfile:
         csvout = csv.writer(outfile)
         
         with driver.session() as session:
+            # Get all attribute types
             results = session.run(
                 "MATCH (:Sample)-[u:hasAttribute]->(a:Attribute) \
                 RETURN a.type AS type, COUNT(u) AS usage_count \
-                ORDER BY usage_count DESC LIMIT 10"
+                ORDER BY usage_count DESC"
             )
             
             for result in results:
                 row = ["{} | {}".format(result["type"], result["usage_count"])]
-                print "{} | ({})".format(result["type"], result["usage_count"])
+                print "{} | {}".format(result["type"], result["usage_count"])
                 csvout.writerow(row)
 
+                print "Generating file of values for attribute type..."
+
+                # For each attribute type, get all values
+                attribute_type_name = result["type"]
+                with driver.session() as session2:
+                    attr_type_values = session2.run(
+                        "MATCH (s:Sample)-->(a:Attribute)-->(t:AttributeType{name:'"+str(attribute_type_name)+"'}), \
+                        (a:Attribute)-->(v:AttributeValue) WITH a,t,v,COUNT(s) AS usage_count \
+                        RETURN a.iri as iri, t.name AS type, v.name AS value, usage_count \
+                        ORDER BY usage_count DESC"
+                    )
+
+                    # prepare file for output of attribute value information
+                    attribute_type_filename = attribute_type_name.lower()
+                    attribute_type_filename = attribute_type_filename.replace(" ", "_")
+                    attribute_type_filename = attribute_type_filename+".csv"
+                    save_directory_path = os.getcwd()
+                    data_directory = "attr_type_values-data_results"
+                    completeName = os.path.join(save_directory_path, data_directory, attribute_type_filename)  
+                    # open file to write data
+                    att_type_values_data = open(completeName, "w")
+
+                    for attr_type_value in attr_type_values:
+                        value = attr_type_value["value"].encode('utf-8')
+                        value_count = attr_type_value["usage_count"]
+                        value_iri = attr_type_value["iri"]
+                        
+                        if value_iri is None:
+                            value_iri = "none"
+                        else:
+                            value_iri = value_iri.encode('utf-8')
+
+                        att_type_values_data.write(value+"\t"+str(value_count)+"\t"+value_iri+"\n")
+                print "\n"
+
+
     print "** DONE - generated file of most common attribute types."
+    print "** DONE - generated files with all values for each attribute type."
+
+
 
 
 if __name__ == "__main__":
@@ -65,14 +105,11 @@ if __name__ == "__main__":
     Generate file of most common Attribute Types and their count. 
     """
     driver = GraphDatabase.driver("bolt://localhost", auth=basic_auth("neo4j", "neo4jebi"))
-    get_all_attribute_types(driver)
     
-    # driver = GraphDatabase.driver("bolt://localhost", auth=basic_auth("neo4j", "neo4jebi"))
+    get_all_attribute_types_and_values(driver)
+    
+    # get_values_for_attribute_type(results)
 
-    # #spreadsheet of most common attribute types and values
-    # print "Generating spreadsheet of most common attribute types and values..."
-    # start_time = time.time()
-    # print "START:", start_time
 
     # with open("TEST-attr_common.csv", "w") as outfile:
     #     csvout = csv.writer(outfile)
@@ -83,11 +120,12 @@ if __name__ == "__main__":
     #             RETURN a.type AS type, COUNT(u) AS usage_count \
     #             ORDER BY usage_count DESC LIMIT 3000"
     #         )
-            
+              # Get all attribute types
     #         for result in results:
     #             print "{} ({})".format(result["type"], result["usage_count"])
     #             row = ["{} | {}".format(result["type"], result["usage_count"])]
 
+                  # For each attribute type, get all values
     #             attribute_type_name = result["type"]
     #             with driver.session() as session2:
     #                 results2 = session2.run(
@@ -96,7 +134,7 @@ if __name__ == "__main__":
     #                     RETURN a.iri as iri, t.name AS type, v.name AS value, usage_count \
     #                     ORDER BY usage_count DESC LIMIT 8000"
     #                 )
-                    
+
     #                 # prepare file for output
     #                 attribute_type_filename = attribute_type_name.lower()
     #                 attribute_type_filename = attribute_type_filename.replace(" ", "_")
@@ -119,11 +157,4 @@ if __name__ == "__main__":
     #                     att_type_out.write(type_value+"\t"+str(type_count)+"\t"+type_iri+"\n")
     #             print "\n"
     #             csvout.writerow(row)
-    
-    # end_time = time.time()
-    # print "END:", end_time
-    # total_time = end_time - start_time           
-    
-    # print "** DONE - generated spreadsheet (attr_common.csv) of most common attribute types and values in %s seconds" % total_time
-    # print "Time in minutes", total_time/60
 
