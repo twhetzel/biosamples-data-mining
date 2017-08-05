62e41a6e2143f73c56491532e79c206d0aeebd1a
Trish Whetzel <plwhetzel@gmail.com>
2017-08-05 16:52:48 +0100

    search for exact match, fallback to any match; format search results

--- scripts/ols_search_annotations.py
+++ scripts/ols_search_annotations.py
@@ -13,8 +13,39 @@ class DataFormatter:
     Format results into JSON for further analysis.
     """
 
-    def __init__(self, data):
+    def __init__(self, attr_type, num_results, data):
+        self.attr_type = attr_type
+        self.num_results = num_results
         self.data = data
+
+
+    def create_term_result_obj(self):
+        formatted_attr_type = self.attr_type.lower()
+        formatted_attr_type = formatted_attr_type.replace(" ", "_")
+
+        value_obj = {}
+        results_list = []
+        
+        for term_result in self.data:
+            result_obj = {}
+
+            keys = term_result.keys()
+            op_key = "ontology_prefix"
+            label_key = "label"
+            iri_key = "iri"
+
+            if op_key in keys:
+                result_obj[op_key] = term_result[op_key]
+            if label_key in keys:
+                result_obj[label_key] = term_result[label_key]
+            if iri_key in keys:
+                result_obj[iri_key] = term_result[iri_key]
+            results_list.append(result_obj)
+
+        value_obj["number_of_ols_results"] = self.num_results
+        value_obj["results"] = results_list
+
+        return value_obj
 
 
 def get_ontology_topics():
@@ -73,14 +104,15 @@ def ols_search(attribute_type_dict, ontology=None):
     """
 
     # Search with entire attr type with &local=true&exact=true, 
-    params1 = "&local=true&exact=true"
+    params1 = "local=true&exact=true"
     # if no results found, search with each token (not stopword) &local=true&exact=true
     # if no results, search with entire attr type, &local=true&exact=false
-    params2 = "&local=true&exact=false"
+    params2 = "local=true&exact=false"
     # if no results, search each token (not stopword) &local=true&exact=false
 
     # Results: Get first 10 results returned
     # format fields into list of Json objects
+    ols_result_obj = {}
 
 
     # prepare file for output of attribute value information
@@ -92,7 +124,8 @@ def ols_search(attribute_type_dict, ontology=None):
     
     # open file to write data -> this will prob move outside loop when generating all data
     outfile = open(completeName, "w")
-    csvout = unicodecsv.writer(outfile)
+    # csvout = unicodecsv.writer(outfile)
+    csvout = csv.writer(outfile)
 
     all_attribute_types = attribute_type_dict.keys()
     attr_type_count = 0
@@ -100,23 +133,27 @@ def ols_search(attribute_type_dict, ontology=None):
     for attr_type in all_attribute_types:
         attr_type_count += 1
 
-        csvout.writerow(["Ontology", "Number of Results", attr_type, attribute_type_dict[attr_type]])
-
         attribute_type_formatted = attr_type.lower()
         attribute_type_formatted = attribute_type_formatted.replace(" ", "_")
 
         if attr_type_count <= int(args.num_attr_review):
-            print "\n** Attribute Type: ("+ str(attr_type_count) +")", attr_type, attribute_type_dict[attr_type]
+            print "\n** Attribute Type("+ str(attr_type_count) +"): ", attr_type, attribute_type_dict[attr_type]
 
             # get ols search results
-            terms_found = _get_results(attr_type, params1)
-            if terms_found != "None":
-                csvout.writerow([terms_found])
-            else:
-                terms_found = _get_results(attr_type, params2)
-                csvout.writerow([terms_found])
+            NO_RESULTS = 0
+            formatted_attr_type = attr_type.lower()
+            formatted_attr_type = formatted_attr_type.replace(" ", "_")
 
-            
+            num_results, ols_term_result_obj = _get_results(attr_type, params1)
+
+            if num_results != NO_RESULTS:
+                ols_result_obj[formatted_attr_type] = ols_term_result_obj
+            else:
+                num_results, ols_term_result_obj = _get_results(attr_type, params2)
+                ols_result_obj[formatted_attr_type] = ols_term_result_obj
+    
+    # write ols search results for attr_trype to file
+    csvout.writerow([ols_result_obj])
     outfile.close()
 
 
@@ -127,29 +164,33 @@ def _get_results(attr_type, params):
     OLS_URL = " http://www.ebi.ac.uk/ols/api/search?q={attr_type:s}&" \
                 "{params}".format(attr_type=attr_type, params=params)
 
-    print "\n** ATTR-TYPE, PARAMS: ", attr_type, params
-
     try:
         response = requests.get(OLS_URL)
         if response.status_code == 200:
             results = json.loads(response.content)
             if results:
                 num_results = results["response"]["numFound"]
+                # print "** NumResults: ", num_results
                 if num_results > 0:
-                    # get first 10 results
                     terms_found = results["response"]["docs"]
-                    print "** Terms found: ", terms_found
-                    return terms_found
+                    data_formatter = DataFormatter(attr_type, num_results, terms_found)
+                    return (num_results, data_formatter.create_term_result_obj())
                 else:
-                    print "** No terms found. Try params2."
+                    # print "** No terms found. Try params2."
                     # call method with params2
-                    return "None"
+                    return (num_results,"None")
             else:
-                # print "No reponse"
-                csvout.writerow(["none", "none"])
+                # print "** No reponse!!!"
+                # csvout.writerow(["none", "none"])
+                pass
+        else:
+            print "** RESPONSE STATUS CODE: ", response.status_code
+            if response.status_code == 500:
+                print "\n--> ReTry OLS...", attr_type, params, "\n"
+                _get_results(attr_type, params)
     except requests.exceptions.RequestException as e:
         print e
-        csvout.writerow(e)
+        # csvout.writerow(e)
 
 
 if __name__ == '__main__':
